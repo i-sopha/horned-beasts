4b9a267ece18a522b762312e9887fac4b05282e1
Isabel Sopha <i.sopha94@gmail.com>
2023-04-10 08:11:28 -0700

    some more updates

--- src/components/main.js
+++ src/components/main.js
@@ -15,24 +15,7 @@ class Main extends React.Component {
                 imageUrl= "https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80"
                 description= "Parent rhino with two babies"
                 />
-                <HornedBeasts />
-                <HornedBeasts />
-                <HornedBeasts />
-                <HornedBeasts />
-                <HornedBeasts />
-                <HornedBeasts />
-                <HornedBeasts />
-                <HornedBeasts />
-                <HornedBeasts />
-                <HornedBeasts />
-                <HornedBeasts />
-                <HornedBeasts />
-                <HornedBeasts />
-                <HornedBeasts />
-                <HornedBeasts />
-                <HornedBeasts />
-                <HornedBeasts />
-                <HornedBeasts />
+                
             </>
         )
     }
