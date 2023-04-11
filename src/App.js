58007fadec1b05a33cacf137ed015afd6da336dd
Isabel Sopha <i.sopha94@gmail.com>
2023-04-11 06:07:13 -0700

    finished modal

--- src/App.js
+++ src/App.js
@@ -1,17 +1,91 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Component } from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import Data from './components/data.json';
+import SelectedBeasts from './components/SelectedBeasts';
+// import HornedBeasts from './components/HornedBeasts';
+// Why get an error if HornedBeasts is uncommented?
 
-function App() {
-  return (
-    <div className="App">
-      <Header heading="Magical Horned Beasts" />
-      <Main />
-      <Footer creator="Isabel Sopha"/>
-    </div>
-  );
+// Previously was a function that I changed to a component in order to use a constructor function - due to errors
+
+// Initial State objects starting with empty strings to render what I want to be rendered in Modal
+// show: determines whether SelectedBeasts component should be displayed, in this case it is NOT shown
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+      title: '',
+      src: '',
+      description: ''
+    };
+  }
+
+// Updates 'show' property from false to true, respectively.
+// When an image is clicked from Main.js - the getSelectedMethod(below) is called and updates the 'show' in either 
+  handleClose = () => {
+    this.setState({
+      show: false
+    });
+  }
+
+  handleOpen = () => {
+    this.setState({
+      show: true
+    });
+  }
+
+  // handleClick = () => {
+  //   const newStatus = this.state.show === false ? true : false;
+  //   this.setState({
+  //     show: newStatus
+  //   });
+  // }
+
+// Updates the state upon click and calls handleOpen to show the SelectedBeasts component
+  getSelectedMethod = (title, src, description) => {
+    this.setState({
+      title: title,
+      src: src,
+      description: description,
+    }); 
+    this.handleOpen();
+  };
+
+// Retruning HTML elements for components
+// Main and Selectedbeasts RECEIVING props
+// 'this' - instance of App component - can access methods and properties
+  render() {
+    return (
+      <div className="App">
+
+        <Header heading="Magical Horned Beasts" />
+
+        <Main
+          data={Data}
+          handleOpen={this.handleOpen}
+          getSelectedMethod={this.getSelectedMethod}
+        />
+
+        {/* <HornedBeasts /> */}
+
+        <SelectedBeasts 
+          show={this.state.show}
+          handleClose={this.handleClose}
+          title={this.state.title}
+          src={this.state.src}
+          description={this.state.description}
+        />
+
+        <Footer creator="Isabel Sopha"/>
+
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,37 +2,52 @@ import { Component } from 'react';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import HornedBeast from './HornedBeast';
-import beasts from './images.json';
+import HornedBeasts from './HornedBeasts';
 
+// data prop is passed down from parent component which is App.js - instead of importing it directly here we just grab from parent since parent component needs it there as well ?
+// Using a constructor allows us to easily set the initial state object based on props received from a parent component.
+// Could we still import data.json without using props ? so instead we would put 'data: {Data}' ? 
 class Main extends Component {
 
+  constructor(props) {
+      super(props);
+      this.state = {
+          data: this.props.data,
+      };
+  }
+
+
+  // map() METHOD is used to loop over objects created from HornedBeasts
+  // arrow function expression being used as a callback function
+  // this.state.data is what we want to iterate - taking in a 'beast' argument
+  // returns each element in the array
+
+  // The key prop is used to uniquely identify each Col component in the React virtual DOM.
+  // The key prop is necessary for when rendering a list of elements using map()
+
+  // The handleOpen and getSelectedMethod props are also passed to the HornedBeasts component and are methods defined in the parent component - App - that can be used to update its state.
+  
   render() {
-    return (
-            <>
-                <Container>
-                    <Row>
-                        {beasts.map(beast => {
-                        return (
-                            <Col xs={4}>
-                            <HornedBeast 
-                            key={beast._id} 
-                            title={beast.title} 
-                            imageUrl={beast.image_url} 
-                            description={beast.description} 
+      return (
+        <Container>
+            <Row>
+                {this.state.data.map(beast => {
+                    return (
+                        <Col xs={4} key={beast.title}>
+                            <HornedBeasts 
+                                title={beast.title}
+                                imgUrl={beast.image_url}
+                                description={beast.description}
+                                handleOpen={this.props.handleOpen}
+                                getSelectedMethod={this.props.getSelectedMethod}
                             />
-                            </Col>
-                        )
-                        })
-                        }
-                    </Row>
-                </Container>
-
-
-            </>
-        )
-    }
+                        </Col>
+                    )
+                })}
+            </Row>
+        </Container>
+      );
+  }
 }
-
 
 export default Main;
